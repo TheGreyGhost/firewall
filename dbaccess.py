80f9c1fdd39a51dc87e91a96142977247032a744
The Grey Ghost <5291905+TheGreyGhost@users.noreply.github.com>
2018-07-27 00:18:48 +0930

    testing1

--- dbaccess.py
+++ dbaccess.py
@@ -8,9 +8,16 @@ import time
 
 class DBaccess:
     db = None
-    cursor = None
+    cursor = None   # named tuple cursor into the db which holds access information
     UNKNOWN_MAC_OWNER_NAME = 'unknown'
     DEFAULT_ACCESS = False                  # if the access tables say "Default" then this is the access granted
+
+    BLOCKING_PRIORITY_OWNER_EXPLICIT = 1
+    BLOCKING_PRIORITY_CLIENT_EXPLICIT = 2
+    BLOCKING_PRIORITY_OWNER_TIMETABLE = 3
+    BLOCKING_PRIORITY_CLIENT_TIMETABLE = 4
+    BLOCKING_PRIORITY_OWNER_DEFAULT = 5
+    BLOCKING_PRIORITY_CLIENT_DEFAULT = 6
 
     def __init__(self, username="testreadonly", dbpassword=None,
                  host="localhost", port="8889", dbname="testfirewall"):
@@ -30,7 +37,7 @@ class DBaccess:
                                              db=dbname)
                 # you must create a Cursor object. It will let
                 #  you execute all the queries you need
-                self.cursor = self.db.cursor()
+                self.cursor = self.db.cursor(named_tuple=True)
             except mysql.connector.Error as err:
                 if err.errno == errorcode.ER_ACCESS_DENIED_ERROR and interactive:
                     print("Something is wrong with your user name or password")
@@ -60,31 +67,75 @@ class DBaccess:
         """
 
 #        Logic:
-#            1) check owner: if blocked/unblocked, apply.  Else,
-#            2) check client: if blocked/unblocked, apply.  Else,
-#            3) check timetable.  TODO: If whitelist, check whitelist IPs.
+#            1) check owner: if blocked/unblocked, apply.  Else:
+#            2) check client: if blocked/unblocked, apply.  Else:
+#            3) check timetable - first for owner, if none then check device.  If neither:
+#               TODO: If whitelist, check whitelist IPs.
+#            4) use global default
 
         if self.db is None or self.cursor is None:
             raise DatabaseError("Not connected to a database")
 
         self.cursor.execute("SELECT * FROM qryClientAccess WHERE MAC='{}'".format(macaddress))
 
-        row = self.cursor.fetchone()
-        if row is None:                 # don't know this MAC.  use reserved entry in owners table: unknown
-            self.cursor.execute("SELECT * FROM owners WHERE name='{}'".format(self.UNKNOWN_MAC_OWNER_NAME))
-            row = self.cursor.fetchone()
-            if row is None:
-                raise DatabaseError("unknown MAC default owner {} not found in database".format(self.UNKNOWN_MAC_OWNER_NAME))
-
-
+        # if we don't know this MAC, or the device has no owner, use reserved entry in owners table: unknown
+        clientrow = self.cursor.fetchone()
+        if clientrow is None or clientrow.owner is None:
+            ownername = self.UNKNOWN_MAC_OWNER_NAME
+            self.cursor.execute("SELECT * FROM owners WHERE name='{}'".format(ownername))
+            ownerrow = self.cursor.fetchone()
+            if ownerrow is None:
+                raise DatabaseError("'unknown' owner {} not found in database".format(ownername))
+            owneraccess = self.checkowner(ownerrow.status, ownerrow.endtime, time.time(), ownerrow.timetable)
+            if clientrow is None:
+                return owneraccess[0]
         else:
-            while row is not None:
-                print(row)
-                row = cursor.fetchone()
+            owneraccess = self.checkclient(clientrow.ownerstatus, clientrow.ownerendtime, time.time(),
+                                           clientrow.ownertimetable)
 
-            # print all the first cell of all the rows
-            for row in self.cursor.fetchall():
-                print(row[0])
+        clientaccess = self.checkclient(clientrow.clientstatus, clientrow.clientendtime, time.time(), clientrow.clienttimetable)
+        return owneraccess[0] if owneraccess[1] < clientaccess[1] else clientaccess[0]
+
+    def checkclient(self, clientstatus, clientendtime, timenow, timetable):
+        """ Check if the named client has access or not.
+
+        :param clientstatus: the status of the owner ('Default','BlockedUntil','UnblockedUntil','Timetable')
+        :param clientendtime: for 'BlockedUntil' or 'UnblockedUntil', the end time of the status, in string format
+                                2018-07-20 20:41:48
+        :param timenow: the current time in Python time format
+        :param timetable: the name of the timetable to be applied (Null = no timetable)
+        :returns a tuple: (access, priority) where access==true if access is permitted, and blocking priority:
+        :raises ValueError, DatabaseError
+        """
+
+        #        Logic:
+        #            1) if blocked/unblocked and time hasn't expired, apply it, otherwise fall back to timetable or default
+        if clientstatus == "BlockedUntil" or clientstatus == "UnblockedUntil":
+            timevalid = False
+            try:
+                clientendtimestruct = time.strptime(clientendtime, "%Y-%m-%d %H:%M:%S")
+            except ValueError:
+                errorhandler.logerror(
+                    DatabaseError("endtime {} did not match expected format 2018-07-20 20:41:48".format(clientendtime)))
+            else:
+                timevalid = (timenow < clientendtimestruct)
+
+            if timevalid:
+                return (False, self.BLOCKING_PRIORITY_CLIENT_EXPLICIT) if clientstatus == "BlockedUntil" \
+                    else (True, self.BLOCKING_PRIORITY_CLIENT_EXPLICIT)
+
+            clientstatus2 = "Default" if timetable is None else "Timetable"
+        else:
+            clientstatus2 = clientstatus
+
+        #   2) Use Default or timetable to find access
+        if clientstatus2 == "Default":
+            return (self.DEFAULT_ACCESS, self.BLOCKING_PRIORITY_CLIENT_DEFAULT)
+        elif clientstatus2 == "Timetable":
+            # TODO look up timetable
+            return (False, self.BLOCKING_PRIORITY_CLIENT_TIMETABLE)
+        else:
+            raise (DatabaseError("Invalid clientstatus:{}".format(clientstatus)))
 
 
     def checkowner(self, ownerstatus, ownerendtime, timenow, timetable):
@@ -95,8 +146,8 @@ class DBaccess:
                                 2018-07-20 20:41:48
         :param timenow: the current time in Python time format
         :param timetable: the name of the timetable to be applied (Null = no timetable)
-        :return: true if access is permitted
-
+        :returns a tuple: (access, priority) where access==true if access is permitted, and blocking priority:
+        :raises ValueError, DatabaseError
         """
 
     #        Logic:
@@ -107,20 +158,23 @@ class DBaccess:
                 ownerendtimestruct = time.strptime(ownerendtime, "%Y-%m-%d %H:%M:%S")
             except ValueError:
                 errorhandler.logerror(DatabaseError("endtime {} did not match expected format 2018-07-20 20:41:48".format(ownerendtime)))
-            finally:
-                timevalid = (timenow < ownerendtime)
+            else:
+                timevalid = (timenow < ownerendtimestruct)
 
             if timevalid:
-                return False if ownerstatus == "BlockedUntil" else True
+                return (False, self.BLOCKING_PRIORITY_OWNER_EXPLICIT) if ownerstatus == "BlockedUntil" \
+                                                                      else (True, self.BLOCKING_PRIORITY_OWNER_EXPLICIT)
 
             ownerstatus2 = "Default" if timetable is None else "Timetable"
         else:
             ownerstatus2 = ownerstatus
 
+    #   2) Use Default or timetable to find access
         if ownerstatus2 == "Default":
-            return self.DEFAULT_ACCESS
+            return (self.DEFAULT_ACCESS, self.BLOCKING_PRIORITY_OWNER_DEFAULT)
         elif ownerstatus2 == "Timetable":
             #TODO look up timetable
+            return (False, self.BLOCKING_PRIORITY_OWNER_TIMETABLE)
         else:
             raise(DatabaseError("Invalid ownerstatus:{}".format(ownerstatus)))
 
--- unittests1.py
+++ unittests1.py
@@ -1,15 +1,76 @@
 import unittest
 from dbaccess import DBaccess
 from errorhandler import DatabaseError
+import time
 
 class MyTestCase(unittest.TestCase):
     def test_databaseconnections(self):
-        with DBaccess() as newdb:
-            newdb.testconnection()
+        pass
+        # with DBaccess() as newdb:
+        #     newdb.testconnection()
+        #
+        # with self.assertRaises(DatabaseError):
+        #     with DBaccess(username="invalid", dbpassword="junk") as newdb:
+        #         newdb.testconnection()
 
-        with self.assertRaises(DatabaseError):
-            with DBaccess(username="invalid", dbpassword="junk") as newdb:
-                newdb.testconnection()
+    def test_clientlogic(self):
+        with DBaccess(dbpassword="TESTREADONLY") as db:
+            # 1. BlockedUntil - still valid
+            testtimestr = "2018-06-03 12:35:22"
+            testtime_earlier = time.strptime("2018-06-03 11:35:22", "%Y-%m-%d %H:%M:%S")
+            testtime_later = time.strptime("2018-06-03 13:35:22", "%Y-%m-%d %H:%M:%S")
+            self.assertEqual(db.checkclient("BlockedUntil", testtimestr, testtime_earlier, None),
+                             (False, DBaccess.BLOCKING_PRIORITY_CLIENT_EXPLICIT), "tcl1")
+
+            # 2. BlockedUntil - expired
+            self.assertEqual(db.checkclient("BlockedUntil", testtimestr, testtime_later, None)[1],
+                              DBaccess.BLOCKING_PRIORITY_CLIENT_DEFAULT, "tcl2")
+
+            # 3. UnblockedUntil - still valid
+            self.assertEqual(db.checkclient("UnblockedUntil", testtimestr, testtime_earlier, None),
+                             (True, DBaccess.BLOCKING_PRIORITY_CLIENT_EXPLICIT), "tcl3")
+
+            # 4. UnblockedUntil - expired
+            self.assertEqual(db.checkclient("UnblockedUntil", testtimestr, testtime_later, None)[1],
+                             DBaccess.BLOCKING_PRIORITY_CLIENT_DEFAULT, "tcl4")
+
+            # 5. Timetable
+            self.assertEqual(db.checkclient("Timetable", testtimestr, testtime_later, None)[1],
+                             DBaccess.BLOCKING_PRIORITY_CLIENT_TIMETABLE, "tcl5")
+
+            # 6. Default
+            self.assertEqual(db.checkclient("Default", testtimestr, testtime_later, None)[1],
+                             DBaccess.BLOCKING_PRIORITY_CLIENT_DEFAULT, "tcl6")
+
+    def test_ownerlogic(self):
+        with DBaccess(dbpassword="TESTREADONLY") as db:
+            # 1. BlockedUntil - still valid
+            testtimestr = "2018-06-03 12:35:22"
+            testtime_earlier = time.strptime("2018-06-03 11:35:22", "%Y-%m-%d %H:%M:%S")
+            testtime_later = time.strptime("2018-06-03 13:35:22", "%Y-%m-%d %H:%M:%S")
+            self.assertEqual(db.checkowner("BlockedUntil", testtimestr, testtime_earlier, None),
+                             (False, DBaccess.BLOCKING_PRIORITY_OWNER_EXPLICIT), "tol1")
+
+            # 2. BlockedUntil - expired
+            self.assertEqual(db.checkowner("BlockedUntil", testtimestr, testtime_later, None)[1],
+                             DBaccess.BLOCKING_PRIORITY_OWNER_DEFAULT, "tol2")
+
+            # 3. UnblockedUntil - still valid
+            self.assertEqual(db.checkowner("UnblockedUntil", testtimestr, testtime_earlier, None),
+                             (True, DBaccess.BLOCKING_PRIORITY_OWNER_EXPLICIT), "tol3")
+
+            # 4. UnblockedUntil - expired
+            self.assertEqual(db.checkowner("UnblockedUntil", testtimestr, testtime_later, None)[1],
+                             DBaccess.BLOCKING_PRIORITY_OWNER_DEFAULT, "tol4")
+
+            # 5. Timetable
+            self.assertEqual(db.checkowner("Timetable", testtimestr, testtime_later, None)[1],
+                             DBaccess.BLOCKING_PRIORITY_OWNER_TIMETABLE, "tol5")
+
+            # 6. Default
+            self.assertEqual(db.checkowner("Default", testtimestr, testtime_later, None)[1],
+                            DBaccess.BLOCKING_PRIORITY_OWNER_DEFAULT, "tol6")
+
 
 if __name__ == '__main__':
     unittest.main()
